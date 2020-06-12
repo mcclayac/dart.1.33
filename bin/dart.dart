e14bc29e66355b6f21989e79aae062a26dea44ce
mcclayac <anthony.mcclay@gmail.com>
2020-06-12 15:13:30 -0400

    Initial commit

---
+++ bin/dart.dart
@@ -0,0 +1,33 @@
+//code 1.33
+//main(List<String> arguments) {
+//  int aNum = 12;
+//  double aDouble = 2.25;
+//  var theResult = aNum ~/ aDouble;
+//  print(theResult);
+//}
+
+//code 1.34
+//main(List<String> arguments) {
+//  int aNum = 12;
+//  double aDouble = 2.25;
+//  var theResult = aNum / aDouble;
+//  print(theResult);
+//}
+
+//code 1.35
+//main(List<String> arguments) {
+//  int aNum = 12;
+//  aNum++;
+//  ++aNum;
+//  int anotherNum = aNum + 1;
+//  print(anotherNum);
+//}
+
+//code 1.36
+main(List<String> arguments) {
+  int firstNum = 40;
+  int secondNum = 41;
+  if (firstNum != secondNum){
+    print("$firstNum is not equal to the $secondNum");
+  } else print("$firstNum is equal to the $secondNum");
+}
