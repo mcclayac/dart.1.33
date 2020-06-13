f46127d87a54ce0a4bb8b620cad8a3243d326271
Tony <anthony.mcclay@me.com>
2020-06-13 07:20:15 -0400

    up to code 1.39

--- bin/dart.dart
+++ bin/dart.dart
@@ -33,10 +33,32 @@
 //}
 
 //code 1.37
+//main(List<String> arguments) {
+//  int firstNum = 40;
+//  int secondNum = 40;
+//  if (firstNum == secondNum){
+//    print("$firstNum is equal to the $secondNum");
+//  } else print("$firstNum is not equal to the $secondNum");
+//}
+
+//code 1.38
 main(List<String> arguments) {
   int firstNum = 40;
   int secondNum = 40;
-  if (firstNum == secondNum){
-    print("$firstNum is equal to the $secondNum");
-  } else print("$firstNum is not equal to the $secondNum");
+  int thirdNum = 74;
+  int fourthNum = 56;
+  if (firstNum == secondNum || thirdNum == fourthNum){
+    print("If choice between 'true' or 'false', the 'true' gets the precedence.");
+  } else print("If choice between 'true' or 'false', the 'false' gets the precedence.");
+}
+
+//code 1.39
+main(List<String> arguments) {
+  int firstNum = 40;
+  int secondNum = 40;
+  int thirdNum = 74;
+  int fourthNum = 56;
+  if (firstNum == secondNum && thirdNum == fourthNum){
+    print("If choice between 'true' or 'false', in this case the 'true' gets the precedence.");
+  } else print("If choice between 'true' or 'false', in this case the 'false' gets the precedence.");
 }
