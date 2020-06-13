1c0f320cbb0b320bfa162b153c63b067bc17e0e9
Tony <anthony.mcclay@me.com>
2020-06-13 07:34:28 -0400

    up to code 1.43

--- bin/dart.dart
+++ bin/dart.dart
@@ -42,23 +42,70 @@
 //}
 
 //code 1.38
-main(List<String> arguments) {
-  int firstNum = 40;
-  int secondNum = 40;
-  int thirdNum = 74;
-  int fourthNum = 56;
-  if (firstNum == secondNum || thirdNum == fourthNum){
-    print("If choice between 'true' or 'false', the 'true' gets the precedence.");
-  } else print("If choice between 'true' or 'false', the 'false' gets the precedence.");
-}
+//main(List<String> arguments) {
+//  int firstNum = 40;
+//  int secondNum = 40;
+//  int thirdNum = 74;
+//  int fourthNum = 56;
+//  if (firstNum == secondNum || thirdNum == fourthNum){
+//    print("If choice between 'true' or 'false', the 'true' gets the precedence.");
+//  } else print("If choice between 'true' or 'false', the 'false' gets the precedence.");
+//}
 
 //code 1.39
+//main(List<String> arguments) {
+//  int firstNum = 40;
+//  int secondNum = 40;
+//  int thirdNum = 74;
+//  int fourthNum = 56;
+//  if (firstNum == secondNum && thirdNum == fourthNum){
+//    print("If choice between 'true' or 'false', in this case the 'true' gets the precedence.");
+//  } else print("If choice between 'true' or 'false', in this case the 'false' gets the precedence.");
+//}
+
+//code 1.40
+//main(List<String> arguments) {
+//  int aNUmber = 35;
+//  if(!(aNUmber != 150) && aNUmber <= 150){
+//    print("It's true");
+//  } else print("It's false.");
+//}
+
+//code 1.41
+//main(List<String> arguments) {
+//  int myNumber = 13;
+//  bool isTrue = true;
+//  print(myNumber is int);
+//  print(myNumber is! int);
+//  print(myNumber is! bool);
+//  print(myNumber is bool);
+//}
+
+//code 1.42
+//main(List<String> arguments) {
+//  int firstNum = 10;
+//  int secondNum;
+//  if(firstNum == 10) print("The value of ${firstNum} is set.");
+//  if (secondNum == null) print("It is true.");
+//  secondNum ??= firstNum;
+//  print(secondNum);
+//}
+
+
+//code 1.43
 main(List<String> arguments) {
-  int firstNum = 40;
-  int secondNum = 40;
-  int thirdNum = 74;
-  int fourthNum = 56;
-  if (firstNum == secondNum && thirdNum == fourthNum){
-    print("If choice between 'true' or 'false', in this case the 'true' gets the precedence.");
-  } else print("If choice between 'true' or 'false', in this case the 'false' gets the precedence.");
+  int firstNum = 10;
+  int secondNum;
+  if(firstNum == 10) print("The value of ${firstNum} is set.");
+  if (secondNum == null) print("It is true.");
+  secondNum ??= firstNum;
+  print(secondNum);
+  print("After using an assignment operator, the value changes.");
+  secondNum += secondNum;
+  print(secondNum);
+  print("After using an assignment operator, the value changes again.");
+  secondNum -= secondNum;
+  print(secondNum);
+  if (secondNum == null) print("It is true.");
+  else print("it is false, because the 'secondNUm' has the value of ${secondNum} now.");
 }
