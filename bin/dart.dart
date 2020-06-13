05e691119d9fd0a279a034b935ed6acc450f8adf
mcclayac <anthony.mcclay@gmail.com>
2020-06-13 08:02:33 -0400

    Ureformat code

--- bin/dart.dart
+++ bin/dart.dart
@@ -96,7 +96,7 @@
 main(List<String> arguments) {
   int firstNum = 10;
   int secondNum;
-  if(firstNum == 10) print("The value of ${firstNum} is set.");
+  if (firstNum == 10) print("The value of ${firstNum} is set.");
   if (secondNum == null) print("It is true.");
   secondNum ??= firstNum;
   print(secondNum);
@@ -106,6 +106,9 @@ main(List<String> arguments) {
   print("After using an assignment operator, the value changes again.");
   secondNum -= secondNum;
   print(secondNum);
-  if (secondNum == null) print("It is true.");
-  else print("it is false, because the 'secondNUm' has the value of ${secondNum} now.");
+  if (secondNum == null)
+    print("It is true.");
+  else
+    print(
+        "it is false, because the 'secondNUm' has the value of ${secondNum} now.");
 }
